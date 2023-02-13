be1ff80f55c66e2a0c502e343a3b4b7462193512
chien pham <pcc@globeteam.com>
2023-02-13 17:49:03 +0700

    add more endpoint

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ require('dotenv').config();
 let app = express();
 app.use(express.static('public'));
 
-app.get('/test', async function (req, res, next) {
+app.get('/', async function (req, res, next) {
     try {
         res.send("hello");
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.get('/test', async function (req, res, next) {
+    try {
+        res.json({name: 'chien'});
     } catch (err) {
         next(err);
     }
